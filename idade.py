0958d9c793f3aceb8ae14d82d41ebb819d5cd1c4
Henrique Valente <bravehenrique@gmail.com>
2020-01-26 14:39:25 -0300

    adicionei duas pastas e um software de mensagens para indicada idade

---
+++ idade.py
@@ -0,0 +1,31 @@
+x= int(input('Qual a sua idade? '))
+
+if x > 50:
+	print('Que experiente!')
+else:
+	pass
+
+if x >= 18:
+	print('A habilitação te aguarda!')
+else:
+	print('Só mais um pouquinho para dirigir')
+
+y = None
+
+if x >= 16:
+	y= input('Já votou esse ano? ')
+else:
+	print('Já já vai poder votar!')
+
+if y != None:
+	if y=="sim":
+		z= input("Qual? B ou H? ")
+		if z=="B":
+			print("Bolsominion")
+
+		if z=="H":
+			print("O Lula tá preso babaca!")
+
+	if y=="não" or y=="nao":
+		print("Luciano Huck para presidente")
+
---
+++ programa3.py
@@ -0,0 +1,6 @@
+x = input('Insira seu nome: ')
+print('Olá '+ x)
+if len(x)> 5:
+	print('Uau, que nome grande!')
+else:
+	print('Quanto menor, mais fofo!')	
